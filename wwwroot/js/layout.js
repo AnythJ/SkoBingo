cc05d80cb7e4bc4332a69b8e4de87412f34cff9c
AnythJ <jkb.czelusta@gmail.com>
2022-04-15 01:32:32 +0200

    Top and side navbars.

---
+++ wwwroot/js/layout.js
@@ -0,0 +1,9 @@
+﻿function showNavbar() {
+    document.getElementById("sideNavbar").style.width = "15rem";
+    document.getElementById("closeIcon").style.transform = "rotate(90deg)";
+}
+
+function hideNavbar() {
+    document.getElementById("sideNavbar").style.width = "0";
+    document.getElementById("closeIcon").style.transform = "rotate(-90deg)";
+}
