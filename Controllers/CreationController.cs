01add47e4528f9076c0a235c34c54f6159780c06
AnythJ <jkb.czelusta@gmail.com>
2022-04-14 20:35:22 +0200

    Add project files.

---
+++ Controllers/CreationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkoBingo.Controllers
+{
+    public class CreationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
