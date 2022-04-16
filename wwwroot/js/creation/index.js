d6bb813c3da2267084a418da960f2104e64b5e6a
AnythJ <jkb.czelusta@gmail.com>
2022-04-16 23:09:02 +0200

    Finished size input.

---
+++ wwwroot/js/creation/index.js
@@ -0,0 +1,7 @@
+﻿function sizeUp() {
+    if (document.getElementById("Size").value < 10) document.getElementById("Size").value++;
+}
+
+function sizeDown() {
+    if (document.getElementById("Size").value > 0) document.getElementById("Size").value--;
+}
