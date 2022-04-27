615434aa147c053b5aeb135963e0e3b5d857037c
AnythJ <jkb.czelusta@gmail.com>
2022-04-27 20:36:58 +0200

    Added mark details after question block click with action to mark it. Styled it for now, but size needs fixing mainly for tablets resolutions.

---
+++ wwwroot/js/home/play.js
@@ -0,0 +1,18 @@
+﻿function closeDetails() {
+    document.getElementsByClassName("question-details")[0].style.display = "none";
+}
+
+function markOne(id) {
+    var markBlock = document.getElementById(id);
+    if (window.getComputedStyle(markBlock, null).getPropertyValue("background-color") == "rgb(26, 26, 29)") {
+        markBlock.style.backgroundColor = "var(--bg-secondary)";
+    }
+    else markBlock.style.backgroundColor = "var(--bg-primary)";
+}
+
+function openDetails(text, id) {
+    document.getElementById("detailsText").innerText = text;
+    document.getElementsByClassName("question-details")[0].style.display = "block";
+    document.getElementById("markButton").onclick = function () { markOne(id);};
+}
+
