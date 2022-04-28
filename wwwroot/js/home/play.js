11b779c329d8b02c7d48db546e507cec9eb347e1
AnythJ <jkb.czelusta@gmail.com>
2022-04-28 22:52:06 +0200

    Added main logic to bingo. Fixed some styling issues with bingo block sizes.

--- wwwroot/js/home/play.js
+++ wwwroot/js/home/play.js
@@ -4,15 +4,61 @@
 
 function markOne(id) {
     var markBlock = document.getElementById(id);
+    var size = document.getElementsByClassName("question-block").length;
     if (window.getComputedStyle(markBlock, null).getPropertyValue("background-color") == "rgb(26, 26, 29)") {
         markBlock.style.backgroundColor = "var(--bg-secondary)";
+
+
+        AddMarkedBlock(id, size);
     }
-    else markBlock.style.backgroundColor = "var(--bg-primary)";
+    else {
+        markBlock.style.backgroundColor = "var(--bg-primary)";
+        DeleteMarkedBlock(id, size);
+    }
+
+    closeDetails();
 }
 
 function openDetails(text, id) {
     document.getElementById("detailsText").innerText = text;
-    document.getElementsByClassName("question-details")[0].style.display = "block";
-    document.getElementById("markButton").onclick = function () { markOne(id);};
+    document.getElementsByClassName("question-details")[0].style.display = "flex";
+    document.getElementById("markButton").onclick = function () { markOne(id); };
+}
+
+function AddMarkedBlock(id, size) {
+    var selectedBlocks = JSON.parse(localStorage.getItem("selectedBlocks") || "[]");
+
+    var block = id;
+    selectedBlocks.push(id);
+    localStorage.setItem("selectedBlocks", JSON.stringify(selectedBlocks));
+
+    CheckIfBingo(id, 6);
+}
+
+function DeleteMarkedBlock(id) {
+    var selectedBlocks = JSON.parse(localStorage.getItem("selectedBlocks") || "[]");
+
+    var block = id;
+    selectedBlocks.splice(selectedBlocks.indexOf(id), 1);
+    localStorage.setItem("selectedBlocks", JSON.stringify(selectedBlocks));
+}
+
+function CheckIfBingo(id, size) {
+    let startingIndex = id % size;
+    let verticalCount = 0;
+    let horizontalCount = 0;
+    var selectedBlocks = JSON.parse(localStorage.getItem("selectedBlocks") || "[]");
+    
+    for (var i = startingIndex; i < size*size; i+=size) {
+        if (selectedBlocks.indexOf(String(i)) >= 0) verticalCount++;
+    }
+
+    for (var i = size; i < size+size; i++) {
+        if (selectedBlocks.indexOf(String(i)) >= 0) horizontalCount++;
+    }
+
+    if (horizontalCount == size || verticalCount == size) {
+        alert("Bingo");
+    }
 }
 
